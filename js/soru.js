98c67ddf58bad1c0792fea83f9a743bd1cc8765d
Ege <76093709+EgeGulsen@users.noreply.github.com>
2022-09-19 22:40:17 +0300

    Add files via upload

---
+++ js/soru.js
@@ -0,0 +1,75 @@
+let questions = [
+    {
+    soru_num: 1,
+    question: "AAAA",
+    cevap: "id:82387",
+    seçenekler: [
+      "id:82387",
+      "id:32423",
+      "id:32432",
+      "id:21321",
+      "id:86423"
+    ]
+  },
+    {
+    soru_num: 2,
+    question: "BBBB",
+    cevap: "a",
+    seçenekler: [
+      "a",
+      "b",
+      "c",
+      "d",
+      "e"
+    ]
+  },
+  {
+    soru_num: 3,
+    question: "BBBB",
+    cevap: "a",
+    seçenekler: [
+      "a",
+      "b",
+      "c",
+      "d",
+      "e"
+    ]
+  },
+  {
+    soru_num: 4,
+    question: "BBBB",
+    cevap: "a",
+    seçenekler: [
+      "a",
+      "b",
+      "c",
+      "d",
+      "e"
+    ]
+  },
+  {
+    soru_num: 5,
+    question: "BBBB",
+    cevap: "a",
+    seçenekler: [
+      "a",
+      "b",
+      "c",
+      "d",
+      "e"
+    ]
+  },
+  {
+  soru_num: 6,
+    question: "BBBB",
+    cevap: "a",
+    seçenekler: [
+      "a",
+      "b",
+      "c",
+      "d",
+      "e"
+    ]
+  },
+  
+];
